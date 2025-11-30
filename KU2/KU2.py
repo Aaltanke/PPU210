fe1e7f76af12791ca3f0df936b6bfa90bfc17de6
Hannes Kindahl <hannes.kindahl@proton.me>
2025-11-30 12:01:42 +0100

    Ändrade .xlxs till .csv som koden nu läser, uppgiftsinstruktioner nu inkluderade

--- KU2/KU2.py
+++ KU2/KU2.py
@@ -7,10 +7,14 @@ Created on Thu Nov 27 15:20:02 2025
 
 import matplotlib.pyplot as plt
 import numpy as np
-from numpy import pi, linspace
-import pandas as pd
+from numpy import pi, array
+import csv
 from sympy import symbols
 
+
+    
+
+Fz1_list,t_list=[],[]
 Fax,Fs,Fk,F0, F_N=symbols("F_ax F_s F_k F_0 F_N")
 E_k=symbols("E_k")
 µ,µ_b,r_m= symbols("µ µ_b,r_m")
@@ -26,20 +30,27 @@ d1=20.752e-3
 D_a =L_k+dw
 x=(L_k/D_a)**(1/5)
 # =============================================================================
-
+E=205*1e9 #Pa = 205 GPa
 g=9.82 #m/s^2
 Fz=(hydrosetvikt+axelvikt)*g
 Fskruv=Fz/12
 Askruv=pi*d**2/4
-Agänga=pi/4*(dw**2-dh**2)+pi/8*(D_a-dw)*dw*((x+1)**2-1)
+Aekv=pi/4*(dw**2-dh**2)+pi/8*(D_a-dw)*dw*((x+1)**2-1)
 
 #kraftdata=pd.read_excel("file://H62_UniformFeedLoad.xlsx","H62_data",0,"Fz1",1)
 #Varuns värden
 # =============================================================================
-E=205*1e9 #Pa = 205 GPa
+
+with open('H62_UniformFeedLoad.csv', newline='') as csvfile:
+    spamreader = csv.reader(csvfile, delimiter=';', quotechar='|')
+
+    file_values=array(list(spamreader))[1:]
+    t_list.append(file_values[:,0])
+    Fz1_list.append(file_values[:,1])
 # =============================================================================
+
 c_s=E*Askruv/L_k
-c_k=E*Agänga/L_k
+c_k=E*Aekv/L_k
 D_a=L_k+dw
 x=(L_k/D_a)**(1/5)
 Aekv=pi/4*(dw**2-dh**2)+pi/8*(D_a-dw)*dw*((x+1)**2-1)
@@ -48,12 +59,14 @@ Aekv=pi/4*(dw**2-dh**2)+pi/8*(D_a-dw)*dw*((x+1)**2-1)
 
 
 
+
+
 c_k=Aekv*E/L_k
 ck_lutning=np.arctan(c_k)
 cs_lutning=np.arctan(c_s)
 Z=(0,10,1001)
 
-for z in Z:
+for t in t_list:
     pass
 # =============================================================================
 # M_tot=Fax*(0.16*P+0.58*µ*d2+µ_b*r_m)
