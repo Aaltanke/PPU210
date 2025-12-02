15902783607c7195309a4cbdbe19f84558d4b041
Hannes Kindahl <hannes.kindahl@proton.me>
2025-12-02 23:23:46 +0100

    Har utökat koden och delat in den i flera funktioner som kan senare återanvändas, funktionerna existerar endast i KU2.py

--- KU2/KU2.py
+++ KU2/KU2.py
@@ -5,72 +5,109 @@ Created on Thu Nov 27 15:20:02 2025
 @author: abdia
 """
 
-import matplotlib.pyplot as plt
 import numpy as np
-from numpy import pi, array
+from numpy import pi, array, tan,linspace
 import csv
 from sympy import symbols
+import matplotlib.pyplot as plt
 
+def F_delta(delta_0,c1,c2):
+    X=linspace(0,delta_0,1001)
+    deltas,deltak=[],[]
+    for x in X:
+        deltas.append(c1*x)
+        deltak.append(c2*(delta_0-x))
+    return X, deltas, deltak
 
-    
+def diagram(X,lista1,lista2):
+    plt.plot(X,lista1,label="deltas")
+    plt.plot(X,lista2,label="deltak")
+    plt.legend()
+    plt.ylim(0,2.5e4)
+    plt.show()
 
-Fz1_list,t_list=[],[]
-Fax,Fs,Fk,F0, F_N=symbols("F_ax F_s F_k F_0 F_N")
-E_k=symbols("E_k")
-µ,µ_b,r_m= symbols("µ µ_b,r_m")
-# =============================================================================
-n= 12 #antal skruvar
-hydrosetvikt=2800 #kg
-axelvikt=11e3 #kg
-L_k = 0.076 #m
-P=3
-dw=d=24e-3 #m
-dh=d2=22.051e-3
-d1=20.752e-3
-D_a =L_k+dw
-x=(L_k/D_a)**(1/5)
-# =============================================================================
-E=205*1e9 #Pa = 205 GPa
-g=9.82 #m/s^2
-Fz=(hydrosetvikt+axelvikt)*g
-Fskruv=Fz/12
-Askruv=pi*d**2/4
-Aekv=pi/4*(dw**2-dh**2)+pi/8*(D_a-dw)*dw*((x+1)**2-1)
+def maximalist(input_list):
+    mops=0
+    for l in input_list:
+        if abs(float(l))>mops:
+            mops=abs(float(l))
+    return mops
 
-#kraftdata=pd.read_excel("file://H62_UniformFeedLoad.xlsx","H62_data",0,"Fz1",1)
-#Varuns värden
+def c(E,A,L):
+    return(E*A/L)
+
+def sigma(F,A):
+    return(F/A)
+
+def Moment(Fax,P,µ,d_2,rm):
+    M=Fax*(0.16*P+0.58*µ*d_2+µ*rm)
+    return M
+#Läser filvärden
 # =============================================================================
 
 with open('H62_UniformFeedLoad.csv', newline='') as csvfile:
     spamreader = csv.reader(csvfile, delimiter=';', quotechar='|')
 
     file_values=array(list(spamreader))[1:]
-    t_list.append(file_values[:,0])
-    Fz1_list.append(file_values[:,1])
+    t_list=(file_values[:,0])
+    Fz1_list=(file_values[:,1])
+
+# =============================================================================
+# =============================================================================
+n= 12 #antal skruvar
+hydrosetvikt=2800 #kg
+axelvikt=11e3 #kg
+L_k = 0.076 #m
+P24=3
+dw=d=24e-3 #m
+dh=d2=22.051e-3 #m
+d1=20.752e-3 #m
+E_värde=205*1e9 #Pa = 205 GPa
+g=9.82 #m/s^27-
 # =============================================================================
 
-c_s=E*Askruv/L_k
-c_k=E*Aekv/L_k
+D_a =L_k+dw
+x=(L_k/D_a)**(1/5)
+Fg=(hydrosetvikt+axelvikt)*g
+Fskruv=Fg/12
+Askruv=(pi/16)*(d1+d2)**2
+Aekv=pi/4*(dw**2-dh**2)+pi/8*(D_a-dw)*dw*((x+1)**2-1)    
+
+Fs,Fk,F0, F_N=symbols("F_s F_k F_0 F_N")
+E_k=symbols("E_k")
+µ_min=0.1
+µ_max=0.18
+r_m= (dw+dh)/4
+
+
+Fz_max=maximalist(Fz1_list)
+F_Nmax=Fg+Fz_max
+
+Fs_max=(F_Nmax)/12
+sigmas_max=Fs_max/Askruv
+sigmas_a=(Fs_max+Fskruv)/(2*Askruv)
+
+
+c_s,c_k=c(E_värde,Askruv,L_k),c(E_värde,Aekv,L_k)
+
 D_a=L_k+dw
 x=(L_k/D_a)**(1/5)
 Aekv=pi/4*(dw**2-dh**2)+pi/8*(D_a-dw)*dw*((x+1)**2-1)
 δ_s=Fskruv/c_s
-δ_k=Fk/c_k
+δ_k=Fskruv/c_k
 
+δ_0=δ_s+abs(δ_k)
 
+Fs_max=F_Nmax/12
+F0_max=1.05*(c_k/(c_s+c_k)*(Fg))
 
+Z,Fs_list,Fk_list=F_delta(δ_0,c_s,c_k)
 
-
-c_k=Aekv*E/L_k
-ck_lutning=np.arctan(c_k)
-cs_lutning=np.arctan(c_s)
-Z=(0,10,1001)
-
-for t in t_list:
-    pass
+diagram(Z, Fs_list, Fk_list)
 # =============================================================================
-# M_tot=Fax*(0.16*P+0.58*µ*d2+µ_b*r_m)
-# print(M_tot)
-# =============================================================================
+M_totmin=Moment(F0_max,P24,µ_min,d2,r_m)
+M_totmax=Moment(F0_max,P24,µ_max,d2,r_m)
+print(M_totmax)
 
-plt.plot
+F0_maxmin=M_totmin/(0.16*P24)
+# =============================================================================
