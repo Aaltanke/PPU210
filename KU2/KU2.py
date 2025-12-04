e45c61bb8cb76aa2bd981679268ed3a5ad84ee78
Hannes Kindahl <hannes.kindahl@proton.me>
2025-12-04 18:09:15 +0100

    Uppdatering av koden med mer exakta värden och mindre bloat, Momentet är däremot fel med en faktor på ~9-10 och behöver korrigeras

--- KU2/KU2.py
+++ KU2/KU2.py
@@ -59,55 +59,57 @@ hydrosetvikt=2800 #kg
 axelvikt=11e3 #kg
 L_k = 0.076 #m
 P24=3
-dw=d=24e-3 #m
-dh=d2=22.051e-3 #m
+dw=33.25e-3
+dh=26e-3
+d=24e-3 #m
+d2=22.051e-3 #m
 d1=20.752e-3 #m
 E_värde=205*1e9 #Pa = 205 GPa
-g=9.82 #m/s^27-
+g=9.82 #m/s^2
 # =============================================================================
 
-D_a =L_k+dw
-x=(L_k/D_a)**(1/5)
-Fg=(hydrosetvikt+axelvikt)*g
-Fskruv=Fg/12
-Askruv=(pi/16)*(d1+d2)**2
-Aekv=pi/4*(dw**2-dh**2)+pi/8*(D_a-dw)*dw*((x+1)**2-1)    
+x=(L_k/(L_k+dw))**(1/5)
 
-Fs,Fk,F0, F_N=symbols("F_s F_k F_0 F_N")
-E_k=symbols("E_k")
+Fk=0
+Fg=(hydrosetvikt+axelvikt)*g
+Fskruv=Fg/n
+Fz_max=maximalist(Fz1_list)
+F_Nmax=Fg+Fz_max
+F_NmaxPerSkruv=F_Nmax/n
+
+Askruv=(pi*d**2)/4
+Aekv=pi/4*(dw**2-dh**2)+(pi/8)*L_k*dw*((x+1)**2-1)
+
+Aekv=pi/4*(dw**2-dh**2)+pi/8*L_k*dw*((x+1)**2-1)
+Asp=pi/16*(d1+d2)**2
+
 µ_min=0.1
 µ_max=0.18
 r_m= (dw+dh)/4
 
-
-Fz_max=maximalist(Fz1_list)
-F_Nmax=Fg+Fz_max
-
-Fs_max=(F_Nmax)/12
-sigmas_max=Fs_max/Askruv
-sigmas_a=(Fs_max+Fskruv)/(2*Askruv)
-
+sigmas_max=F_NmaxPerSkruv/Askruv
+sigmas_a=(F_NmaxPerSkruv+Fskruv)/(2*Askruv)
 
 c_s,c_k=c(E_värde,Askruv,L_k),c(E_värde,Aekv,L_k)
 
-D_a=L_k+dw
-x=(L_k/D_a)**(1/5)
-Aekv=pi/4*(dw**2-dh**2)+pi/8*(D_a-dw)*dw*((x+1)**2-1)
 δ_s=Fskruv/c_s
 δ_k=Fskruv/c_k
-
 δ_0=δ_s+abs(δ_k)
-
-Fs_max=F_Nmax/12
-F0_max=1.05*(c_k/(c_s+c_k)*(Fg))
-
+δ_pl=4.3e-6+ 2*6e-6+4.8e-6
 Z,Fs_list,Fk_list=F_delta(δ_0,c_s,c_k)
+
+
+F01=1.05*(Fk+(c_k/(c_s+c_k)*(F_NmaxPerSkruv)))
+F02=((c_k*c_s)*δ_pl)/(c_s+c_k)
+F03=F01+F02
+
+
 
 diagram(Z, Fs_list, Fk_list)
 # =============================================================================
-M_totmin=Moment(F0_max,P24,µ_min,d2,r_m)
-M_totmax=Moment(F0_max,P24,µ_max,d2,r_m)
-print(M_totmax)
+sigma=F03/Asp
+M_min=Moment(F03, P24, µ_min, d2, r_m)
+M_max=Moment(F03, P24, µ_max, d2, r_m)
+F_friktionslös=M_min/(0.16*P24)
 
-F0_maxmin=M_totmin/(0.16*P24)
 # =============================================================================
